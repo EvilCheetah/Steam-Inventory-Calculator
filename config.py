beabf8606947a96af7f5bef13fd14030af74e95a
Eugene M <eugene.moschyn@gmail.com>
2020-07-06 15:17:36 -0700

    Edited config to read and change user's preffered currency.

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import configparser
+import library.CONST.URL as URL
+import library.CONST.SteamData as SteamData
+
 
 class config:
     def __init__(self, PATH: str):
@@ -7,5 +10,9 @@ class config:
         #to make return the keys in Uppercase
         #e.g. dota_2 will be returned as DOTA_2
         self.userConfig.optionxform = str
-
         self.userConfig.read(PATH)
+
+        #Reading userCurrency and Changing it in CONST.URL for the rest of the program
+        URL.CURRENCY = SteamData.CURRENCY[ self.userConfig["USER_DATA"]["CURRENCY"] ]["code"]
+
+        print(URL.CURRENCY)
--- library/CONST/URL.py
+++ library/CONST/URL.py
@@ -1,7 +1,9 @@
 Delimeters = [',', '.', ' ']
 
+CURRENCY   = 0
+
 USER_HEADER 	= {"User-Agent": "Mozilla/5.0 (Linux; Android 10; SC-01M) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/11.2 Chrome/75.0.3770.143 Mobile Safari/537.36"}
-COOKIE			= {"steamLoginSecure":  "76561198133833906%7C%7C55224C6922C109939E0D240079AB0C730789BA11"}
+COOKIE			= {"steamLoginSecure":  "76561198133833906%7C%7C628CC305C045EFBE98EF43C4D799A2C078C77B7A"}
 
 
 MARKET_LISTING 	= ("https://steamcommunity.com/market/priceoverview/?appid={APP_ID}&market_hash_name={NAME_OF_ITEM}&currency={CURRENCY_ID}")
