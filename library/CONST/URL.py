f8f733c1a45ce9018b958a662ed481b16efc6c0b
Eugene M <eugene.moschyn@gmail.com>
2020-07-10 03:32:48 -0700

    Added market price history URL.

--- library/CONST/URL.py
+++ library/CONST/URL.py
@@ -2,10 +2,12 @@ Delimeters = [',', '.', ' ']
 
 CURRENCY   = 0
 
-USER_HEADER 	= {"User-Agent": "Mozilla/5.0 (Linux; Android 10; SC-01M) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/11.2 Chrome/75.0.3770.143 Mobile Safari/537.36"}
-COOKIE			= {"steamLoginSecure":  "76561198133833906%7C%7C628CC305C045EFBE98EF43C4D799A2C078C77B7A"}
+USER_HEADER          = {"User-Agent": "Mozilla/5.0 (Linux; Android 10; SC-01M) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/11.2 Chrome/75.0.3770.143 Mobile Safari/537.36"}
+COOKIE               = {"steamLoginSecure":  ""}
 
 
-MARKET_LISTING 	= ("https://steamcommunity.com/market/priceoverview/?appid={APP_ID}&market_hash_name={NAME_OF_ITEM}&currency={CURRENCY_ID}")
+INVENTORY            = ("https://steamcommunity.com/profiles/{STEAM_ID64}/inventory/json/{APP_ID}/2")
 
-INVENTORY		= ("https://steamcommunity.com/profiles/{STEAM_ID64}/inventory/json/{APP_ID}/2")
+MARKET_LISTING 	     = ("https://steamcommunity.com/market/priceoverview/?appid={APP_ID}&market_hash_name={ITEM_NAME}&currency={CURRENCY_ID}")
+
+MARKET_PRICE_HISTORY = ("https://steamcommunity.com/market/pricehistory/?appid={APP_ID}&market_hash_name={ITEM_NAME}&currency={CURRENCY_ID}")
